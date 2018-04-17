cd317c3ddbe81e2b50251a057b8e6625cc29ffce
fgiannesini <fabien.giannesini@gmail.com>
2018-04-17 17:43:50 +0200

    Add files via upload
    Simple backpropagate equivalent to derivative

--- NeuralNetwork/backPropagate.m
+++ NeuralNetwork/backPropagate.m
@@ -1,6 +1,5 @@
 function [newTheta] = backPropagate(theta,layerSize,inputData, output)
-  computedOutput = zeros(size(inputData,1), layerSize(length(layerSize)));
-  newTheta = zeros(size(theta));
+  thetaGrad = zeros(size(theta));
   for inputIndex = 1: size(inputData,1)
     result = inputData(inputIndex,:);
     layerResult = zeros(length(layerSize),max(layerSize));
@@ -10,22 +9,20 @@ function [newTheta] = backPropagate(theta,layerSize,inputData, output)
       firstLayerSize = layerSize(layerIndex);
       secondLayerSize = layerSize(layerIndex + 1);
       thetaLayer = reshape(theta(layerIndex, (1:firstLayerSize * secondLayerSize)), firstLayerSize, secondLayerSize);
-      newResult = result * thetaLayer;
-      layerResult(layerIndex + 1,1:length(newResult)) = newResult;
-      result = 1./(1 + exp(-newResult)) ;
+      result = 1./(1 + exp(-result * thetaLayer)) ;
+      layerResult(layerIndex + 1,1:length(result)) = result;
     end;
-    computedOutput(inputIndex,:) = result;
     
     layerError = zeros(length(layerSize),max(layerSize));
-    layerError(length(layerSize),:) = layerResult(length(layerSize),:);
+    layerError(length(layerSize), 1:size(output, 2)) = layerResult(length(layerSize), 1:size(output, 2)) - output(inputIndex,:);
     for layerIndex = length(layerSize)-1:-1:1
       firstLayerSize = layerSize(layerIndex);
       secondLayerSize = layerSize(layerIndex + 1);
       thetaLayer = reshape(theta(layerIndex, (1:firstLayerSize * secondLayerSize)), firstLayerSize, secondLayerSize);
-      layerError(layerIndex,1:firstLayerSize) = thetaLayer * layerError(layerIndex + 1,1:secondLayerSize)' .* layerResult(layerIndex,1:firstLayerSize)'.*(1-layerResult(layerIndex,1:firstLayerSize))';
+      layerError(layerIndex,1:firstLayerSize) = layerError(layerIndex + 1,1:secondLayerSize) * thetaLayer' .* layerResult(layerIndex,1:firstLayerSize).*(1-layerResult(layerIndex,1:firstLayerSize));
       delta = layerError(layerIndex+1,1:secondLayerSize)' * layerResult(layerIndex,1:firstLayerSize);
-      newTheta(layerIndex,1:firstLayerSize * secondLayerSize) += delta(:)';
+      thetaGrad(layerIndex,1:firstLayerSize * secondLayerSize) += delta(:)'./size(inputData,1);
     end;
   end;
-  newTheta./size(inputData,1);
+  newTheta = theta - thetaGrad;
 end;
--- NeuralNetwork/neuralNetwork.m
+++ NeuralNetwork/neuralNetwork.m
@@ -2,7 +2,7 @@ clc;
 clear all;
 close all;
 
-data = load('mnist.mat');
+%data = load('mnist.mat');
 
 %data.trainX is a (60000,784) matrix which contains the pixel data for training
 %data.trainY is a (1,60000) matrix which contains the labels for the training data
@@ -13,7 +13,7 @@ data = load('mnist.mat');
 %i=reshape(X(1,:),28,28)';
 %image(i)
 
-layerSize = [1 6 3];
+layerSize = [1 3 3];
 input = [[1];[2];[3]];
 output = [[1,0,0];[0,1,0];[0,0,1]];
 
@@ -27,14 +27,18 @@ for layerIndex = 1:length(layerSize)-1
   theta(layerIndex, 1:layerCount) = rand(1,layerCount);
 end;
 
+%theta = [1,2,0,0,0,0;3,4,5,6,7,8];
+
 computedOutput = computeOutput(input, layerSize, theta);
 cout(1) = computeCost(output, computedOutput);
-for coutIndex=2:100
-  %thetaWithDerivative = derivateCost(theta, layerSize, input, output);
-  thetaWithBackprop = backPropagate(theta, layerSize, input, output);
-  theta = thetaWithBackprop;
+
+for coutIndex = 2:5000
+%  thetaDerivative = derivateCost(theta, layerSize, input, output);
+  theta = backPropagate(theta, layerSize, input, output);
+  
   computedOutput = computeOutput(input, layerSize, theta);
   cout(coutIndex) = computeCost(output, computedOutput);
+  
 %  if abs(cout(coutIndex) - cout(coutIndex-1)) < 0.0001
 %    break;
 %  end;
