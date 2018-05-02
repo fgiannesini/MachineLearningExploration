b1f72e2f6d809648a80c58cebfb90ebcc7d0cec5
fabien.giannesini <fabien.giannesini@gmail.com>
2018-05-02 08:18:39 +0200

    Start tuning

--- NeuralNetwork/computeCost.m
+++ NeuralNetwork/computeCost.m
@@ -1,4 +1,4 @@
-function [cout] = computeCost(output, computedOutput,theta, regularizationCoeff)
+function [cout] = computeCost(output, computedOutput, theta, regularizationCoeff)
   cout = output.* log(computedOutput) + (1-output) .* log(1-computedOutput);
   cout = - 1 / size(output,1) * sum(cout(:));
   cout += regularizationCoeff / (2*size(output,1)) * sum(theta(:) .* theta(:));
